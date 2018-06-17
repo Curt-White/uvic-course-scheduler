50a4f1ec9995ba7e75b6a34d7a8e222c5e212a92
Curt White <curtwhite@Curts-MacBook-Pro.local>
2018-06-16 21:25:00 -0700

    added angular

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template, send_file, request
 import dbCreate, dbFetchUI, generateSchedules
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
---
+++ frontend/src/app/selectOptions/select.ts
@@ -0,0 +1,8 @@
+export class selectOption{
+    constructor(
+      public name: string,
+      public fos: string,
+      public num: string,
+    ){}
+  } 
+  
